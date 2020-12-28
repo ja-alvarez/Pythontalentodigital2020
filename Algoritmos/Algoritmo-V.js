ebd7725951d4c88a0c2be617a08c1e7367daa27b
Juan Alvarez <juan.alvarez1989@gmail.com>
2020-12-28 20:15:17 -0300

    git updated, Api and Ajax added - 201228

--- Algoritmos/Algoritmo-V.js
+++ Algoritmos/Algoritmo-V.js
@@ -12,7 +12,7 @@ function resetNegativos(b) {
 console.log(resetNegativos([1, 2, -1, -3]))
 
 /*2. Dado un array, mueve todos los valores un espacio de derecha a izquierda eliminando el primer valor y dejando un 0 para el último valor.Por ejemplo, moverAdelante([1, 2, 3]) debiera dar como resultado[2, 3, 0].*/
-
+//a
 function arr(x) {
     b = [];
     b.push(x[1]);
@@ -23,9 +23,26 @@ function arr(x) {
 x = arr([1, 2, 3]);
 console.log(x)
 
+//b
+function moverAdelante(array) {
+    ade = [];
+    for (var i = 1; i < array.length; i++) {
+        (array[0] >= 1); {
+            ade.push(array[i]);
+        }
+    }
+    ade.push(0);
+    return array;
+}
+x = moverAdelante([1, 2, 3]);
+console.log(ade);
+
+
+
 /*3. Configura un array para que el resultado sean los valores en el orden contrario.
 Por ejemplo, returnReverso([1, 2, 3]) debe dar[3, 2, 1].*/
 
+//a
 function returnReverso(x) {
     b = [];
     b.push(x[2]);
@@ -35,6 +52,18 @@ function returnReverso(x) {
 }
 x = returnReverso([1, 2, 3]);
 /*console.log(x)*/
+
+//b
+function returnReverso(arr) {
+    ret = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+        ret.push(arr[i]);
+    }
+    return arr;
+}
+x = returnReverso([1, 2, 3]);
+console.log(ret);
+
 
 /*4. Crea una función que cambie un array repitiendo sus valores originales(manteniendo el mismo orden).
 Por ejemplo, repetirValores([4, ”Ulysses”, 42, false]) debiera dar[4, 4, “Ulysses”, “Ulysses”, 42, 42, 
